fb048d20c4cb3dd12719332b3fa4f4294859d6c2
Wiktoria Sowa <wiktoriasowa99@gmail.com>
2025-02-06 20:15:32 +0100

    Fix history page

--- src/app/history/[number]/page.tsx
+++ src/app/history/[number]/page.tsx
@@ -1,18 +1,14 @@
 import React, { Suspense } from 'react';
-import delayedString from '@/app/history/data';
 
 interface PageProps {
   params: { [key: string]: string };
 }
 
 async function Page({ params }: PageProps) {
-  const result = await delayedString();
   return (
     <div>
       <Suspense fallback={<p>Loading</p>}>
-        <p>
-          Event nr {params.number} {result}
-        </p>
+        <p>Event nr {params.number}</p>
       </Suspense>
     </div>
   );
