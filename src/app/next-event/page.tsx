b3b9e770c7df42b25248280684f6825836f31c4b
Wiktoria Sowa <wiktoriasowa99@gmail.com>
2025-02-14 09:24:54 +0100

    Add submit presentation section to next-event page

--- src/app/next-event/page.tsx
+++ src/app/next-event/page.tsx
@@ -1,10 +1,12 @@
 import NoNextEventSection from '@/components/sections/noNextEvent';
+import AddIdeaSection from '@/components/sections/addIdeaSection';
 
 export default function NextEventPage() {
   // TODO: display next event if present in db
   return (
     <div>
       <NoNextEventSection />
+      <AddIdeaSection />
     </div>
   );
 }
--- src/components/sections/noNextEvent.tsx
+++ src/components/sections/noNextEvent.tsx
@@ -4,15 +4,15 @@ import Link from 'next/link';
 export default function NoNextEventSection() {
   return (
     <div className={styles.flexContainer}>
+      <div className={styles.image}>
+        <img src="/empty-calendar.png" width="350px" />
+      </div>
       <div className={styles.text}>
         <h1>We’re planning something exciting!</h1>
         <p>While you wait, check out our past events and presentations! 🔥</p>
         <Link href={'/history'}>
           <button className={`${styles.secondaryBtn} ${styles.btn}`}>Past events</button>
         </Link>
-      </div>
-      <div className={styles.image}>
-        <img src="/empty-calendar.png" width="350px" />
       </div>
     </div>
   );
